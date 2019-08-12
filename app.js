132d759a6098c5c8a493f86cdcf4841047ae130f
Jung Liu <realumbrally@gmail.com>
2019-08-12 16:03:08 +0800

    Optimize

--- app.js
+++ app.js
@@ -3,13 +3,7 @@ const app = express()
 const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
-const URL = require('./models/url.js')
-const { urlValidator } = require('./lib/validator.js')
-const { validationResult } = require('express-validator')
-let root = 'https://pure-ocean-86993.herokuapp.com'
-if (process.env.NODE_ENV !== 'production') {
-  root = 'localhost:3000'
-}
+
 
 // const port = 3000
 // 連線mongoDB
@@ -33,7 +27,7 @@ app.use('/', require('./routes/home.js'))
 app.use('/url', require('./routes/url.js'))
 
 app.listen(process.env.PORT || 3000, "0.0.0.0", () => {
-  console.log('web server is running', process.env.PORT)
+  console.log('web server is running')
 })
 
 
